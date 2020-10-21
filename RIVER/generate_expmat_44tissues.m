06e2c895a77f515e2244daf55d8a13152166f2b1
katsudon16 <sucipto.kathleen@gmail.com>
2020-10-21 19:04:38 -0400

    Modify the code for RIVER step 1

--- RIVER/generate_expmat_44tissues.m
+++ RIVER/generate_expmat_44tissues.m
@@ -1,7 +1,7 @@
-dirNames.RAREVARDIR = '${RAREVARDIR}'
+dirNames.RAREVARDIR = './'
 
-load(sprintf('%s/preprocessing/dataGTEx_052516.mat',dirNames.RAREVARDIR));
-load(sprintf('%s/preprocessing/annoGTEx_052516.mat',dirNames.RAREVARDIR));
+load(sprintf('%s/reference/dataGTEx.mat',dirNames.RAREVARDIR));
+load(sprintf('%s/reference/annoGTEx.mat',dirNames.RAREVARDIR));
 list_tissues = data.tisNames;
 
 nGenes = length(anno.chr);
@@ -12,9 +12,9 @@ target_data.exp = [];
 matrix.gene_idx = repmat([1:nGenes]',1,nInds);
 matrix.ind_idx = repmat([1:nInds],nGenes,1);
 target_data.all = [reshape(matrix.gene_idx,nGenes*nInds,1) reshape(matrix.ind_idx,nGenes*nInds,1)];
-for tis = 1:length(list_tissues)    
+for tis = 1:length(list_tissues)   
     % You might need to check if you can succefully upload expression data and convert missing values into NaN for matlab environments
-    temp_exp = importdata(sprintf('%s/preprocessing/peer/%s.peer.ztrans.txt',dirNames.RAREVARDIR,list_tissues{tis}));
+    temp_exp = importdata(sprintf('%s/preprocessing/PEER/dat/%s.peer.ztrans.txt',dirNames.RAREVARDIR,list_tissues{tis}));
     if size(temp_exp.data,2) ~= size(temp_exp.textdata,2)-1,
         temp_exp.data = [temp_exp.data NaN(size(temp_exp.data,1),size(temp_exp.textdata,2)-size(temp_exp.data,2)-1)];
     end
@@ -23,12 +23,17 @@ for tis = 1:length(list_tissues)
     
     idx_wgs = NaN(2,length(data.indNames.wgs)); % new | old 
     for i = 1:length(data.indNames.wgs)
-        idx_wgs(:,i) = [i; strmatch(data.indNames.wgs(i),temp_exp.textdata(1,2:end),'exact')];
+        match = strmatch(data.indNames.wgs(i),temp_exp.textdata(1,2:end),'exact');
+        if isempty(match) == 0,
+            idx_wgs(:,i) = [i match];
+        end
+        %idx_wgs(:,i) = [i; strmatch(data.indNames.wgs(i),temp_exp.textdata(1,2:end),'exact')];
     end
+    idx_wgs(:,sum(isnan(idx_wgs)) > 0) = []
     
     idx_gene = NaN(length(anno.chr),2); % new | old
     for i = 1:length(anno.chr)
-        temp_idx = strmatch(anno.gene_ids(i,2),temp_exp.textdata(2:end,1),'exact');
+        temp_idx = strmatch(anno.gene_ids(i),temp_exp.textdata(2:end,1),'exact');
         if length(temp_idx) == 1,
             idx_gene(i,:) = [i temp_idx];
         end
